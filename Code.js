ea3ea1626c6b0e2059426689a00b530e68f25d16
georgesilv3 <george.silv3@gmail.com>
2025-01-11 16:06:45 -0400

    Implementing the function to save on google sheets

--- Code.js
+++ Code.js
@@ -7,3 +7,27 @@ function doGet(e) {
 function include(file){
   return HtmlService.createHtmlOutputFromFile(file).getContent()
 }
+
+function save(client){
+  console.log("entrou")
+  const ss = SpreadsheetApp.getActive()
+  const tab = ss.getSheetByName("agendamentos")
+  const lastRow = tab.getLastRow()
+  const lastColumn = tab.getLastColumn()
+  const header = tab.getRange(1, 1, 1, lastColumn).getValues()[0]
+  console.log(header, lastColumn, lastRow)
+  for (data in client){
+    console.log(data)
+    const index = headerSearcher(header, header.length, data)
+    tab.getRange(lastRow+1, index).setValue(client[data])
+  }
+}
+
+function headerSearcher(header, len, data){
+  for (let index = 0; index < len; index++){
+    if (header[index] == data){
+      console.log(index)
+      return index+1
+    }
+  }
+}
